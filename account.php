4ab4a0ae917a235ab26f6a43511031bd6b870a64
OmarChouchane <omar.ch52831@gmail.com>
2024-03-28 07:38:56 +0100

    logout
    account logout activation

--- account.php
+++ account.php
@@ -1,3 +1,33 @@
+<?php
+
+
+session_start();
+
+
+//if user is not logged in
+if(!isset($_SESSION['logged_in'])){
+
+    header('location: login.php');
+    exit();
+
+}
+
+
+if(isset($_GET['logout'])){
+
+    unset($_SESSION['logged_in']);
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_name']);
+    unset($_SESSION['user_email']);
+    header('location: login.php');
+    exit();
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,10 +87,10 @@
                 <h3 class="font-weight-bold">Account info</h3>
                 <hr class="mx-auto">
                 <div class="account-info">
-                    <p>Name <span>Jhon</span></p>
-                    <p>Email <span>jhon@mail.com</span></p>
-                    <p><a href="" id="orders-btn">Your orders</a></p>
-                    <p><a href="" id="logout-btn">Logout</a></p>
+                    <p>Name : <span><?php if(isset($_SESSION['user_name'])){echo $_SESSION['user_name'];}?></span></p>
+                    <p>Email : <span><?php if(isset($_SESSION['user_email'])){echo $_SESSION['user_email'];}?></span></p>
+                    <p><a href="#orders" id="orders-btn">Your orders</a></p>
+                    <p><a href="account.php?logout=1" id="logout-btn">Logout</a></p>
                 </div>
             </div>
             <div class="mt-3 pt-5 col-lg-6 col-md-12 col-sm-12">
@@ -88,7 +118,7 @@
 
 
     <!--Orders-->
-    <section class="cart container my-5 py-5">
+    <section id="orders" class="cart container my-5 py-5">
         <div class="container text-center">
             <h2 class="font-weight-bold">Your Orders</h2>
             <hr class="mx-auto">
