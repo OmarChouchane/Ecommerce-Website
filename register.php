957cf569c405a50e2efd12da0fd4bf7c33e29470
OmarChouchane <omar.ch52831@gmail.com>
2024-03-28 06:32:44 +0100

    register
    activation of the registration successfully

--- register.php
+++ register.php
@@ -1,3 +1,77 @@
+<?php
+
+
+session_start();
+
+include 'server/connection.php';
+
+if(isset($_POST['register'])){
+
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+
+    // Check if passwords do not match
+    if($password != $confirmPassword){ 
+        header('location: register.php?error=passwords do not match');
+    }
+
+
+    // Check if password is less than 6 characters long
+    else if(strlen($password) < 6){  
+        header('location: register.php?error=password must be at least 6 characters long');
+    }
+
+
+    // If there is no error
+    else{
+
+
+        // Check if the email already exists in the database
+        $stmt1 = $conn->prepare("SELECT count(*) FROM users WHERE user_email = ?"); 
+        $stmt1->bind_param('s', $email);
+        $stmt1->execute();
+        $stmt1->bind_result($num_rows);
+        $stmt1->store_result();
+        $stmt1->fetch();
+
+
+
+        // If the email already exists
+        if($num_rows > 0){
+            header('location: register.php?error=user with this email already exists');
+        }
+
+
+
+        // Insert user info into the users table
+        else{
+            $stmt = $conn->prepare("INSERT INTO users (user_name, user_email, user_password) VALUES (?,?,?)");
+            $stmt->bind_param('sss', $name, $email, md5($password)); 
+
+
+            // If the query is successful
+            if($stmt->execute()){ 
+                $_SESSION['user_email'] = $email;
+                $_SESSION['user_name'] = $name;
+                $_SESSION['logged_in'] = true;
+                header('location: account.html?register=You registered successfully');
+            }else{
+                header('location: register.php?error=could not create account at the moment, try again later');
+            }
+        }
+    }
+}
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -52,11 +126,14 @@
     <!--Register-->
     <section class="my-5 py-5">
         <div class="container text-center mt-3 pt-5">
-            <h2 class="form-weihggt-bold">Register</h2>
+            <h2 class="form-weight-bold">Register</h2>
             <hr class="mx-auto">
         </div>
         <div class="mx-auto container">
-            <form id="register-form">
+            <form id="register-form" method="POST" action="register.php">
+                <?php if(isset($_GET['error'])){?>
+                    <p style="color: red;"><?php echo $_GET['error']; ?></p>
+                <?php } ?>
                 <div class="form-group">
                     <label>Name</label>
                     <input type="text" class="form-control" id="register-email" name="name" placeholder="Name" required>
@@ -74,7 +151,7 @@
                     <input type="password" class="form-control" id="register-confirm-password" name="confirmPassword" placeholder="Confirm Password" required>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn" id="register-btn" value="Register">
+                    <input type="submit" name="register" class="btn" id="register-btn" value="Register">
                 </div>
                 <div class="form-group">
                     <a id="login-url" class="btn" href="">Do you have an account ? Login</a>
