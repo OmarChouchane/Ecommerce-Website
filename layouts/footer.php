05a7360bfebd124af0f2525028e8ede353050d78
OmarChouchane <omar.ch52831@gmail.com>
2024-03-29 12:17:31 +0100

    navbar and footer layout
    new folder layout containing header and footer

---
+++ layouts/footer.php
@@ -0,0 +1,64 @@
+    <!--Footer-->
+    <footer class="mt-5 pt-5">
+        <div class="row container mx-auto pt-5">
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <img src="assets/imgs/logo2.png" class="logo2" alt="">
+                <p class="pt-3">We provide the best products the  most affordable prices</p>
+                <img class="logo3" src="/assets/imgs/logo3.png">
+            <img class="logo3" src="/assets/imgs/logo4.png">
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Featured</h5>
+                <ul>
+                    <li><a href="#">men</a></li>
+                    <li><a href="#">women</a></li>
+                    <li><a href="#">boys</a></li>
+                    <li><a href="#">girls</a></li>
+                    <li><a href="#">new arrivals</a></li>
+                    <li><a href="#">clothes</a></li>
+                </ul>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Contact Us</h5>
+                <div>
+                    <h6>Address</h6>
+                    <p>1234 Street Name, City</p>
+                </div>
+                <div>
+                    <h6>Phone</h6>
+                    <p>12 345 678</p>
+                </div>
+                <div>
+                    <h6>Email</h6>
+                    <p>contact@gmail.com</p>
+                </div>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Products</h5>
+                <div class="row">
+                    <ul>
+                        <li><a href="#">shoes</a></li>
+                        <li><a href="#">watches</a></li>
+                        <li><a href="#">coats</a></li>
+                        <li><a href="#">dresses</a></li>
+                        <li><a href="#">bags</a></li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="copyright mt-5">
+            <div class="row container mx-auto text-center">
+                    <p>eCommerce @ 2025 All Rights Reserved to Maher Jaziri</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
