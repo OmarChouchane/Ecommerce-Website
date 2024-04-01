3a4daf59898d272b821ce41177880fe19782d28d
OmarChouchane <omar.ch52831@gmail.com>
2024-04-01 01:15:03 +0100

    admin page design
    sidebar menu

--- admin/index.php
+++ admin/index.php
@@ -27,6 +27,8 @@ if(!isset($_SESSION['admin_logged_in'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 
 
     <link rel="stylesheet" href="../assets/css/style.css">
@@ -34,20 +36,19 @@ if(!isset($_SESSION['admin_logged_in'])){
     <title>The North Face</title>
     <link rel="icon" href="../assets/imgs/logo22.png" type="image/x-icon">
 </head>
-<body>
+<body id="body-pd" class="body-pd">
 
 
-        <!--Navbar-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
+
+
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
             <div class="container">
                 <img class="logo" src="../assets/imgs/logo2.png">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+            <div class="nav-buttons">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php?logout=1" id="logout-btn">Sign Out</a>
+                <li class="nav-item admin">
+                    <span>Admin.</span>
                 </li>
                 </ul>
             </div>
@@ -55,7 +56,33 @@ if(!isset($_SESSION['admin_logged_in'])){
     </nav>
 
 
+
+    <!--Sidebar-->
+    <section id="sidebar">
+        <div class="l-navbar mt-5 pt-5 show" id="nav-bar">
+            <nav class="nav">
+                <div> 
+                    <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> </div>
+                </div> 
+                <a href="logout.php?logout=1" class="nav_link sign-out"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">Sign Out</span> </a>
+            </nav>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+
+    <script src="script.js"></script>
 
 </body>
 </html>
